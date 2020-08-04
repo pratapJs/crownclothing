162bab4477dd293cfa5d9b67c86d36c44e39ae20
savip <pratapadhikari81@gmail.com>
2020-08-04 23:16:31 +0930

    signin page done

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/HomePage';
 import Shop from './pages/shop/Shop';
+import Header from './components/Header/Header';
+import SigninSignup from './pages/Signin-Signup/SigninSignup';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={Shop} />
+          <Route path='/signin' component={SigninSignup} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/pages/Signin-Signup/SigninSignup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Signin from '../../components/signin/SignIn';
+
+const SigninSignup = () => {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <Signin />
+    </div>
+  );
+};
+
+export default SigninSignup;
